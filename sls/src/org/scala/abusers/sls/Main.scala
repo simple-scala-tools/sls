38c713d3e7c465c4ee7fdc9f481e7981d5c33994
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-05-03 02:33:30 +0200

    Invoke bloopInstall on start

--- sls/src/org/scala/abusers/sls/Main.scala
+++ sls/src/org/scala/abusers/sls/Main.scala
@@ -1,6 +1,9 @@
 package org.scala.abusers.sls
 
 import cats.effect.*
+import fs2.io.process.ProcessBuilder
+import fs2.io.process.Processes
+import fs2.text
 import jsonrpclib.fs2.*
 import langoustine.lsp.*
 import langoustine.lsp.all.*
@@ -14,7 +17,10 @@ object MyServer extends LangoustineApp.Simple:
     LSPBuilder
       .create[IO]
       .handleRequest(initialize) { in =>
+        val rootUri  = in.params.rootUri.toOption.getOrElse(sys.error("what now?"))
+        val rootPath = os.Path(java.net.URI.create(rootUri.value).getPath())
         sendMessage(in.toClient, "ready to initialise!") *>
+          importBsp(rootPath, in.toClient) *>
           IO {
             InitializeResult(
               capabilities = ServerCapabilities(textDocumentSync = Opt(TextDocumentSyncKind.Full)),
@@ -29,8 +35,36 @@ object MyServer extends LangoustineApp.Simple:
         }
       }
 
+  def importBsp(rootPath: os.Path, back: Communicate[IO]) =
+    val alreadyInstalled = os.exists(rootPath / ".bloop")
+    if !alreadyInstalled then
+      ProcessBuilder("./mill", "mill.bsp.BSP/install")
+        .withWorkingDirectory(fs2.io.file.Path.fromNioPath(rootPath.toNIO))
+        .spawn[IO]
+        .use { process =>
+          val logStdout = process.stdout
+          val logStderr = process.stderr
+
+          val allOutput = logStdout
+            .merge(logStderr)
+            .through(text.utf8.decode)
+            .through(text.lines)
+
+          allOutput
+            .evalMap(s => logMeesage(back, s))
+            .compile
+            .drain
+        }
+    else IO.unit
+
   def sendMessage(back: Communicate[IO], msg: String) =
     back.notification(
       window.showMessage,
       ShowMessageParams(MessageType.Info, msg),
+    )
+
+  def logMeesage(back: Communicate[IO], message: String): IO[Unit] =
+    back.notification(
+      window.logMessage,
+      LogMessageParams(MessageType.Info, message),
     )
