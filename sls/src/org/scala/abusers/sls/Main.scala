eb633c165462ce26cd42807e69e189373217bc75
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-05-03 22:11:59 +0200

    Connect with bloop

--- sls/src/org/scala/abusers/sls/Main.scala
+++ sls/src/org/scala/abusers/sls/Main.scala
@@ -1,68 +1,124 @@
 package org.scala.abusers.sls
 
+import cats.data.OptionT
 import cats.effect.*
+import cats.syntax.all.*
+import fs2.concurrent.Topic
 import fs2.io.process.ProcessBuilder
 import fs2.io.process.Processes
 import fs2.text
+import fs2.Chunk
 import jsonrpclib.fs2.*
 import langoustine.lsp.*
 import langoustine.lsp.all.*
 import langoustine.lsp.app.*
 
-object MyServer extends LangoustineApp.Simple:
-  override def server =
-    IO.ref(Set.empty[String]).map(myLSP)
+import java.nio.charset.StandardCharsets
+import java.util.UUID
 
-  def myLSP(files: Ref[IO, Set[String]]) =
+object MyServer extends LangoustineApp.Simple:
+
+  private case class State(files: Set[String], bloopConn: Option[BloopConnection]):
+    def release: IO[Unit]                          = OptionT.fromOption[IO](bloopConn).semiflatTap(_.cancel).value.void
+    def withBloopConnection(conn: BloopConnection) = copy(bloopConn = Some(conn))
+
+  private case class BloopConnection(send: String => IO[Either[Topic.Closed, Unit]], cancel: IO[Unit])
+
+  override def server =
+    Resource
+      .make(IO.ref(State(Set.empty, none)))(ref =>
+        ref
+          .getAndUpdate(_.copy(bloopConn = None))
+          .map(_.release)
+      )
+      .use(myLSP.andThen(IO(_)))
+
+  private def myLSP(stateRef: Ref[IO, State]): LSPBuilder[IO] =
     LSPBuilder
       .create[IO]
       .handleRequest(initialize) { in =>
         val rootUri  = in.params.rootUri.toOption.getOrElse(sys.error("what now?"))
         val rootPath = os.Path(java.net.URI.create(rootUri.value).getPath())
-        sendMessage(in.toClient, "ready to initialise!") *>
-          importMillBsp(rootPath, in.toClient) *>
-          IO {
-            InitializeResult(
-              capabilities = ServerCapabilities(textDocumentSync = Opt(TextDocumentSyncKind.Full)),
-              serverInfo = Opt(InitializeResult.ServerInfo("My first LSP!")),
-            )
-          }
+        for
+          _         <- sendMessage(in.toClient, "ready to initialise!")
+          _         <- importMillBsp(rootPath, in.toClient)
+          bloopConn <- connectWithBloop(in.toClient)
+          _         <- logMeesage(in.toClient, "Connection with bloop estabilished")
+          _         <- stateRef.update(_.withBloopConnection(bloopConn))
+        yield InitializeResult(
+          capabilities = ServerCapabilities(textDocumentSync = Opt(TextDocumentSyncKind.Full)),
+          serverInfo = Opt(InitializeResult.ServerInfo("My first LSP!")),
+        )
       }
       .handleNotification(textDocument.didOpen) { in =>
         val documentUri = in.params.textDocument.uri.value
-        files.updateAndGet(_ + documentUri).map(_.size).flatMap { count =>
-          sendMessage(in.toClient, s"In total, $count files registered!")
+        stateRef.updateAndGet(state => state.copy(files = state.files + documentUri)).map(_.files.size).flatMap {
+          count =>
+            sendMessage(in.toClient, s"In total, $count files registered!")
         }
       }
 
-  def importMillBsp(rootPath: os.Path, back: Communicate[IO]) =
-    val millExec = "./mill" // TODO if mising then findMillExec()
-    ProcessBuilder(millExec, "--import", "ivy:com.lihaoyi::mill-contrib-bloop:", "mill.contrib.bloop.Bloop/install")
-      .withWorkingDirectory(fs2.io.file.Path.fromNioPath(rootPath.toNIO))
-      .spawn[IO]
-      .use { process =>
-        val logStdout = process.stdout
-        val logStderr = process.stderr
-
-        val allOutput = logStdout
-          .merge(logStderr)
+  private def connectWithBloop(back: Communicate[IO]): IO[BloopConnection] =
+    val temp       = os.temp.dir(prefix = "sls")
+    val socketFile = temp / s"bloop-${UUID.randomUUID()}.socket"
+    (for
+      bspSocketProc <- ProcessBuilder("bloop", "bsp", "--socket", socketFile.toNIO.toString())
+        .spawn[IO]
+      topic <- Resource.eval(Topic[IO, Chunk[Byte]])
+    yield (bspSocketProc, topic)).allocated
+      .flatMap { case ((process, topic), cancel) =>
+        val logStream = process.stdout
+          .merge(process.stderr)
           .through(text.utf8.decode)
           .through(text.lines)
+          .evalMap(s => logMeesage(back, s"[bloop] $s"))
 
-        allOutput
-          .evalMap(s => logMeesage(back, s))
+        val consumer = topic.subscribeUnbounded.unchunks
+          .through(process.stdin)
+          .void
+
+        logStream
+          .mergeHaltBoth(consumer)
+          .onFinalizeCase(c => sendMessage(back, s"Bloop process terminated $c"))
           .compile
           .drain
+          .start
+          .map(logFiber =>
+            BloopConnection(
+              str => topic.publish1(Chunk.array(str.getBytes(StandardCharsets.UTF_8))),
+              logFiber.cancel >> cancel,
+            )
+          )
       }
 
-  def sendMessage(back: Communicate[IO], msg: String) =
-    back.notification(
-      window.showMessage,
-      ShowMessageParams(MessageType.Info, msg),
-    )
+def importMillBsp(rootPath: os.Path, back: Communicate[IO]) =
+  val millExec = "./mill" // TODO if mising then findMillExec()
+  ProcessBuilder(millExec, "--import", "ivy:com.lihaoyi::mill-contrib-bloop:", "mill.contrib.bloop.Bloop/install")
+    .withWorkingDirectory(fs2.io.file.Path.fromNioPath(rootPath.toNIO))
+    .spawn[IO]
+    .use { process =>
+      val logStdout = process.stdout
+      val logStderr = process.stderr
 
-  def logMeesage(back: Communicate[IO], message: String): IO[Unit] =
-    back.notification(
-      window.logMessage,
-      LogMessageParams(MessageType.Info, message),
-    )
+      val allOutput = logStdout
+        .merge(logStderr)
+        .through(text.utf8.decode)
+        .through(text.lines)
+
+      allOutput
+        .evalMap(s => logMeesage(back, s))
+        .compile
+        .drain
+    }
+
+def sendMessage(back: Communicate[IO], msg: String): IO[Unit] =
+  back.notification(
+    window.showMessage,
+    ShowMessageParams(MessageType.Info, msg),
+  ) *> logMeesage(back, msg)
+
+def logMeesage(back: Communicate[IO], message: String): IO[Unit] =
+  back.notification(
+    window.logMessage,
+    LogMessageParams(MessageType.Info, message),
+  )
