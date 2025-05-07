d2e8ade1993869aa908f43831bf017c37d89b8d5
Jakub Kozłowski <kubukoz@gmail.com>
2025-05-07 22:01:19 +0200

    uncancellable allocated

--- sls/src/org/scala/abusers/sls/Steward.scala
+++ sls/src/org/scala/abusers/sls/Steward.scala
@@ -1,7 +1,7 @@
 package org.scala.abusers.sls
 
 import cats.effect.kernel.Concurrent
-import cats.effect.kernel.MonadCancel
+import cats.effect.kernel.MonadCancelThrow
 import cats.effect.kernel.Ref
 import cats.effect.kernel.Resource
 import cats.syntax.all.*
@@ -16,11 +16,11 @@ object Steward:
     }
     .map(Impl(_))
 
-  private type MonadCancelThrow[F[_]] = MonadCancel[F, Throwable]
-
   private case class Impl[F[_]: MonadCancelThrow](state: Ref[F, List[F[Unit]]]) extends Steward[F]:
 
     def acquire[A](resource: Resource[F, A]): F[A] =
-      resource.allocated.flatMap { case (a, finalizer) =>
-        state.update(finalizer :: _).as(a)
+      MonadCancelThrow[F].uncancelable { poll =>
+        poll(resource.allocated).flatMap { case (a, finalizer) =>
+          state.update(finalizer :: _).as(a)
+        }
       }
