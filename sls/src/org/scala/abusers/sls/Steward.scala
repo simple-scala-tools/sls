44678b42f73b21effb9e9692ad610b37acb7f8a3
Jakub Kozłowski <kubukoz@gmail.com>
2025-05-07 22:02:14 +0200

    propagate exit case

--- sls/src/org/scala/abusers/sls/Steward.scala
+++ sls/src/org/scala/abusers/sls/Steward.scala
@@ -4,6 +4,7 @@ import cats.effect.kernel.Concurrent
 import cats.effect.kernel.MonadCancelThrow
 import cats.effect.kernel.Ref
 import cats.effect.kernel.Resource
+import cats.effect.kernel.Resource.ExitCase
 import cats.syntax.all.*
 
 trait Steward[F[_]]:
@@ -11,16 +12,16 @@ trait Steward[F[_]]:
 
 object Steward:
   def apply[F[_]: Concurrent]: Resource[F, Steward[F]] = Resource
-    .make(Ref.of(List.empty[F[Unit]])) { finalizers =>
-      finalizers.get.flatMap(_.sequence_)
+    .makeCase(Ref.of(List.empty[ExitCase => F[Unit]])) { (finalizers, exit) =>
+      finalizers.get.flatMap(_.traverse_(_(exit)))
     }
     .map(Impl(_))
 
-  private case class Impl[F[_]: MonadCancelThrow](state: Ref[F, List[F[Unit]]]) extends Steward[F]:
+  private case class Impl[F[_]: MonadCancelThrow](state: Ref[F, List[ExitCase => F[Unit]]]) extends Steward[F]:
 
     def acquire[A](resource: Resource[F, A]): F[A] =
       MonadCancelThrow[F].uncancelable { poll =>
-        poll(resource.allocated).flatMap { case (a, finalizer) =>
+        poll(resource.allocatedCase).flatMap { case (a, finalizer) =>
           state.update(finalizer :: _).as(a)
         }
       }
